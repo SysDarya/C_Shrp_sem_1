72d24a55e98a02815ba6498756ff9887f1c56d83
Sys Darya <sysdaryaandreevna@gmail.com>
2022-09-30 20:42:25 +0300

    task001

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// a = 5; b = 25 -> да 
+// a = 2 b = 10 -> нет 
+// a = 9; b = -3 -> нет 
+// a = -3 b = 9 -> да
+// 2. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//	3 -> Среда 
+// 5 -> Пятница
+
+Console.WriteLine ("Введите первое число: ");
+int a = int.Parse (Console.ReadLine() ?? "0");
+Console.WriteLine ("Введите второе число: ");
+int b = int.Parse (Console.ReadLine() ?? "0");
+if (a == b*b)
+{
+    Console.WriteLine ($"Число {a} является квадратом {b}.");
+}
+else if (b == a*a)
+{
+    Console.WriteLine ($"Число {b} является квадратом {a}.");
+}
+else
+{
+    Console.WriteLine ($"Числа не являются квадратами друг друга.");
+}
+
